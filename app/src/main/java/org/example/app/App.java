bbfaa87cb32522e4f7aa71c255c3d9ac64e962f6
Domingo Suarez Torres <domingo.suarez@gmail.com>
2025-02-14 22:48:39 +0000

    code cleanup

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -32,7 +32,7 @@ public class App {
         final TextBox txtNum2 = new TextBox().setValidationPattern(Pattern.compile("[0-9]*")).addTo(panel);
 
         panel.addComponent(new EmptySpace(new TerminalSize(0, 0)));
-        new Button("Add!", () -> extracted(txtNum1, txtNum2, lblOutput))
+        new Button("Add!", () -> updateOutput(txtNum1, txtNum2, lblOutput))
                 .addTo(panel);
 
         new Button("Quit", () -> System.exit(0))
@@ -41,15 +41,15 @@ public class App {
         panel.addComponent(new EmptySpace(new TerminalSize(0, 0)));
         panel.addComponent(lblOutput);
 
-        txtNum1.setTextChangeListener((s, user) -> {
-            if (user) {
-                extracted(txtNum1, txtNum2, lblOutput);
+        txtNum1.setTextChangeListener((newText, changedByUserInteraction) -> {
+            if (changedByUserInteraction) {
+                updateOutput(txtNum1, txtNum2, lblOutput);
             }
         });
 
-        txtNum2.setTextChangeListener((s, user) -> {
-            if (user) {
-                extracted(txtNum1, txtNum2, lblOutput);
+        txtNum2.setTextChangeListener((newText, changedByUserInteraction) -> {
+            if (changedByUserInteraction) {
+                updateOutput(txtNum1, txtNum2, lblOutput);
             }
         });
 
@@ -62,7 +62,7 @@ public class App {
         gui.addWindowAndWait(window);
     }
 
-    private static void extracted(TextBox txtNum1, TextBox txtNum2, Label lblOutput) {
+    private static void updateOutput(TextBox txtNum1, TextBox txtNum2, Label lblOutput) {
         int num1 = getNum1(txtNum1);
         int num2 = getNum1(txtNum2);
         lblOutput.setText(Integer.toString(num1 + num2));
